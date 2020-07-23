8eac4919c764f85d3269721c0c49e055f4f13d79
allanDavidProjects <allandavidcomode@gmail.com>
2020-07-23 20:55:34 -0300

    implementing swagger configuration and replace the old project name for the new project name

--- src/main/java/com/syst/trades/model/ProductSale.java
+++ src/main/java/com/syst/trades/model/ProductSale.java
@@ -1,12 +1,16 @@
 package com.syst.trades.model;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -53,6 +57,9 @@ public class ProductSale {
 
 	@Column(name = "update_date")
 	private Date updateDate;
+	
+	@OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products = Collections.emptyList();
 
 	public Long getId() {
 		return id;
--- src/main/java/com/syst/trades/repository/ProductSaleRepository.java
+++ src/main/java/com/syst/trades/repository/ProductSaleRepository.java
@@ -1,9 +1,14 @@
 package com.syst.trades.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.syst.trades.model.Product;
 import com.syst.trades.model.ProductSale;
 
 public interface ProductSaleRepository extends JpaRepository<ProductSale, Long> {
+	
+	List<Product> findByProductsName(String name);
 
 }
