73d8aaeff8a4e165d48970eacaa2357ff42f577a
Allan David A. Albuquerque <33264656+davidalbuquerque52@users.noreply.github.com>
2020-07-24 01:06:59 -0300

    Update SecurityAccessConfig.java
    add necessary import

--- src/main/java/com/syst/trades/config/SecurityAccessConfig.java
+++ src/main/java/com/syst/trades/config/SecurityAccessConfig.java
@@ -3,6 +3,7 @@ package com.syst.trades.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 @EnableWebSecurity
